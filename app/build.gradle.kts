d7582c7386ba35f5c217cf88b525e7d3d9474b4f
Riyaz Siddiqui <riyaz.siddiqui@premitivekey.com>
2026-03-02 14:35:32 +0530

    Initial commit with proper gitignore

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.premitivekey.demoretrofit"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.premitivekey.demoretrofit"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+// Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson)
+
+// Lifecycle
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+// Coroutines
+    implementation(libs.coroutines.android)
+}
