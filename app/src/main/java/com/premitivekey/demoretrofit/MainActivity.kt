cb4b902998ff78d112a5f194e307d84a1d37a6b2
Riyaz Siddiqui <riyaz.siddiqui@premitivekey.com>
2026-03-02 14:52:25 +0530

    temp changes

--- app/src/main/java/com/premitivekey/demoretrofit/MainActivity.kt
+++ app/src/main/java/com/premitivekey/demoretrofit/MainActivity.kt
@@ -16,6 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
